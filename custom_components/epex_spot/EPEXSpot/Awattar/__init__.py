3cba08ad34d30a1efd92853d0bc91bc13cbc1c18
brambo123 <52667932+brambo123@users.noreply.github.com>
2026-09-21 13:37:38 +0200

    Cleanup Awattar

--- custom_components/epex_spot/EPEXSpot/Awattar/__init__.py
+++ custom_components/epex_spot/EPEXSpot/Awattar/__init__.py
@@ -13,22 +13,6 @@ from ...const import EUR_PER_MWH, UOM_EUR_PER_KWH
 
 _LOGGER = logging.getLogger(__name__)
 
-
-class AwattarMarketprice(Marketprice):
-    """Marketprice class for Awattar."""
-
-    def __init__(self, data):
-        assert data["unit"].lower() == EUR_PER_MWH.lower()
-        self._start_time = datetime.fromtimestamp(
-            data["start_timestamp"] / 1000, tz=timezone.utc
-        )
-        self._end_time = datetime.fromtimestamp(
-            data["end_timestamp"] / 1000, tz=timezone.utc
-        )
-        self._market_price_per_kwh = round(float(data["marketprice"]) / 1000.0, 6)
-        self._unit = UOM_EUR_PER_KWH
-
-
 def toEpochMilliSec(dt: datetime) -> int:
     return int(dt.timestamp() * 1000)
 
@@ -88,5 +72,15 @@ class Awattar:
     def _extract_marketdata(self, data) -> List[Marketprice]:
         entries = []
         for entry in data:
-            entries.append(AwattarMarketprice(entry))
+            assert entry["unit"].lower() == EUR_PER_MWH.lower()
+            start_time = datetime.fromtimestamp(entry["start_timestamp"] / 1000, tz=timezone.utc)
+            end_time = datetime.fromtimestamp(entry["end_timestamp"] / 1000, tz=timezone.utc)
+            price = round(float(entry["marketprice"]) / 1000.0, 6)
+            entries.append(
+                Marketprice(
+                    start_time=start_time,
+                    end_time=end_time,
+                    price=price
+                )
+            )
         return entries
